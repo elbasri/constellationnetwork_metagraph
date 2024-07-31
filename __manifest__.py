dacb3c910964da86be7169b885f496036dafc31f
Elbasri Abdennacer <devnasser@gmail.com>
2024-08-01 00:57:07 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
         Module to manage metagraphs and their configurations for Constellation Network
     """,
     'author': 'ABDENNACER Elbasri',
-    'depends': ['base', 'mail', 'sale', 'stock', 'payment'],
+    'depends': ['base', 'mail', 'sale', 'stock', 'payment', 'website'],
     'data': [
         'views/metagraph_views.xml',
         'security/ir.model.access.csv',
